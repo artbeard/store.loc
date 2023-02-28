9f74cc4e42ae3a6d39b494992438275ebeda9702
artbeard <76746834+artbeard@users.noreply.github.com>
2023-02-28 15:26:12 +0600

    Добавлены сщности таблиц и репозитории

---
+++ src/Entity/Balance.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\BalanceRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: BalanceRepository::class)]
+class Balance
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\OneToOne(cascade: ['persist', 'remove'])]
+    private ?Product $product = null;
+
+    #[ORM\Column]
+    private ?int $amount = null;
+
+    #[ORM\Column]
+    private ?int $cost = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $balance_at = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getProduct(): ?Product
+    {
+        return $this->product;
+    }
+
+    public function setProduct(?Product $product): self
+    {
+        $this->product = $product;
+
+        return $this;
+    }
+
+    public function getAmount(): ?int
+    {
+        return $this->amount;
+    }
+
+    public function setAmount(int $amount): self
+    {
+        $this->amount = $amount;
+
+        return $this;
+    }
+
+    public function getCost(): ?int
+    {
+        return $this->cost;
+    }
+
+    public function setCost(int $cost): self
+    {
+        $this->cost = $cost;
+
+        return $this;
+    }
+
+    public function getBalanceAt(): ?\DateTimeInterface
+    {
+        return $this->balance_at;
+    }
+
+    public function setBalanceAt(\DateTimeInterface $balance_at): self
+    {
+        $this->balance_at = $balance_at;
+
+        return $this;
+    }
+}
---
+++ src/Entity/Statement.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\StatementRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: StatementRepository::class)]
+class Statement
+{
+    const POST_IN = 'income';
+	const POST_EX = 'expense';
+	
+	#[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\OneToOne(cascade: ['persist', 'remove'])]
+    private ?Product $product = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $document_prop = null;
+
+    #[ORM\Column(length: 10)]
+    private ?string $post_type = Statement::POST_IN;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $posted_at = null;
+
+    #[ORM\Column]
+    private ?int $amount = 0;
+
+    #[ORM\Column]
+    private ?int $cost = 0;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getProduct(): ?Product
+    {
+        return $this->product;
+    }
+
+    public function setProduct(?Product $product): self
+    {
+        $this->product = $product;
+
+        return $this;
+    }
+
+    public function getDocumentProp(): ?string
+    {
+        return $this->document_prop;
+    }
+
+    public function setDocumentProp(string $document_prop): self
+    {
+        $this->document_prop = $document_prop;
+
+        return $this;
+    }
+
+    public function getPostType(): ?string
+    {
+        return $this->post_type;
+    }
+
+    public function setPostType(string $post_type): self
+    {
+        $this->post_type = $post_type;
+
+        return $this;
+    }
+
+    public function getPostedAt(): ?\DateTimeInterface
+    {
+        return $this->posted_at;
+    }
+
+    public function setPostedAt(\DateTimeInterface $posted_at): self
+    {
+        $this->posted_at = $posted_at;
+
+        return $this;
+    }
+
+    public function getAmount(): ?int
+    {
+        return $this->amount;
+    }
+
+    public function setAmount(int $amount): self
+    {
+        $this->amount = $amount;
+
+        return $this;
+    }
+
+    public function getCost(): ?int
+    {
+        return $this->cost;
+    }
+
+    public function setCost(int $cost): self
+    {
+        $this->cost = $cost;
+
+        return $this;
+    }
+}
