0d421b35c5c75061e6703f97d5483bb4189ac149
artbeard <frolov.web@gmail.com>
2023-02-28 23:29:40 +0600

    тестовые методы

--- src/Controller/ApiIncomigController.php
+++ src/Controller/ApiIncomigController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\StatementTypes;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -19,11 +20,21 @@ class ApiIncomigController extends AbstractController
 	    ]);
     }
     
-	#[Route('/api/incomig', name: 'app_api_incomig_add', methods: ['POST'])]
+	#[Route('/api/incomig', name: 'app_api_add_incomig', methods: ['POST'])]
 	public function add_incomig(Request $request, Statement $statementService): Response
 	{
+
+		$statementService->addPost($request->toArray());
 		
-		
+		return $this->json($request->toArray());
+	}
+
+	#[Route('/api/expense', name: 'app_api_add_expense', methods: ['POST'])]
+	public function add_expense(Request $request, Statement $statementService): Response
+	{
+
+		$statementService->addPost($request->toArray(), StatementTypes::POST_EX);
+
 		return $this->json($request->toArray());
 	}
 }
