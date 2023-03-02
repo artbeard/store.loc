29a19a852a6ab921ed6493082ae14ab947dc56ad
artbeard <76746834+artbeard@users.noreply.github.com>
2023-03-02 16:07:40 +0600

    добавлен экшен для проводки предзаказа

--- src/Controller/ApiPreorderController.php
+++ src/Controller/ApiPreorderController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 
+use App\Service\Normalizer;
 use App\Service\Preorder;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -12,19 +13,41 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class ApiPreorderController extends AbstractController
 {
-    #[Route('/api/preorder', name: 'app_api_preorder',  methods: 'GET')]
-    public function index(Preorder $preorderService): JsonResponse
+ 
+	#[Route('/api/preorder', name: 'app_api_preorder',  methods: 'GET')]
+    public function index(Preorder $preorderService, Normalizer $normalizer): JsonResponse
     {
         $preorders = $preorderService->getPreorders();
-        dump($preorders); exit();
-    	return $this->json($preorders);
+    	return $this->json(
+		    $normalizer->normalizeArray($preorders)
+	    );
     }
 	
 	#[Route('/api/preorder', name: 'app_api_preorder_add', methods: 'POST')]
-	public function add_preorder(Request $request): JsonResponse
+	public function add_preorder(Request $request, Preorder $preorderService): JsonResponse
 	{
+		$preorderData = $request->toArray();
+		$preorder = $preorderService->addPreorder($preorderData);
 		return $this->json([
-			'id' => 152,
+			'id' => $preorder->getId(),
 		], Response::HTTP_CREATED);
 	}
+	
+	/**
+	 * @throws \App\Exception\ApiException
+	 */
+	#[Route('/api/preorder/expense', name: 'app_api_preorder_expense', methods: 'POST')]
+	public function preorder_expense(Request $request, Preorder $preorderService): JsonResponse
+	{
+		$preorderParams = $request->toArray();
+		$preorder = $preorderService->postPreorder(
+			$preorderParams['preorder_id'] ?? 0,
+			$preorderParams['price'] ?? 0
+		);
+		return $this->json([
+			$preorder->getId()
+		]);
+	}
+	
+	
 }
