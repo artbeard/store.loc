31be13e9c1cba03991d47f2f147ec283bd79eba6
artbeard <76746834+artbeard@users.noreply.github.com>
2023-03-02 16:11:27 +0600

    Добавлен поиск по неисполненным заказам

--- src/Repository/PreorderRepository.php
+++ src/Repository/PreorderRepository.php
@@ -44,38 +44,12 @@ class PreorderRepository extends ServiceEntityRepository
 	{
 		$est = $date->add(\DateInterval::createFromDateString('-1 day'));
 		return $this->createQueryBuilder('p')
-            ->andWhere('p.sent_at = :sentAt')
-			->orWhere('p.ordered_at = :orderedAt')
-            ->setParameter('sentAt', $date->format('Y-m-d'))
-			->setParameter('orderedAt', $est->format('Y-m-d'))
+			->where('p.sent_at = :sentAt')
+			->orWhere('(p.ordered_at < :sentAt AND p.sent_at IS NULL )')
+			->setParameter('sentAt', $date->format('Y-m-d'))
             ->orderBy('p.id', 'ASC')
-            //->setMaxResults(10)
             ->getQuery()
             ->getResult()
         ;
 	}
-//    /**
-//     * @return Preorder[] Returns an array of Preorder objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('p')
-//            ->andWhere('p.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('p.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
-
-//    public function findOneBySomeField($value): ?Preorder
-//    {
-//        return $this->createQueryBuilder('p')
-//            ->andWhere('p.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
 }
