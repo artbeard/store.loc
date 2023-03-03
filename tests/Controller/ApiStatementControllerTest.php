b3c6036b72eebe36d57eaf0ab08a57a85c22617c
artbeard <76746834+artbeard@users.noreply.github.com>
2023-03-03 11:29:18 +0600

    Добавлен тест для некорректного добавления проводки

--- tests/Controller/ApiStatementControllerTest.php
+++ tests/Controller/ApiStatementControllerTest.php
@@ -16,11 +16,14 @@ class ApiStatementControllerTest extends WebTestCase
         $this->assertTrue(is_array(json_decode($client->getResponse()->getContent(), true)));
     }
 
+    /**
+     * нормальное добавление проводки
+     */
     public function testAddIncome():void
     {
         $headers = ['X-Current-Time' => '2021-01-01', 'Accept' => 'application/json'];
         $postData = [
-            'product_id' => 16,
+            'product_id' => 1,
             'amount'    => 10,
             'cost'      => 100,
             'document_prop' => 'TestPost '.time()
@@ -36,6 +39,32 @@ class ApiStatementControllerTest extends WebTestCase
         );
         $this->assertEquals(Response::HTTP_CREATED, $client->getResponse()->getStatusCode());
         $this->assertArrayHasKey('id', json_decode($client->getResponse()->getContent(), true));
+    }
+
+    /**
+     * Добавление провдки, вызывающее ошибку
+     */
+    public function testAddIncomeFalseProduct():void
+    {
+        $headers = ['X-Current-Time' => '2021-01-01', 'Accept' => 'application/json'];
+        $postData = [
+            'product_id' => time(),
+            'amount'    => 10,
+            'cost'      => 100,
+            'document_prop' => 'TestPost '.time()
+        ];
+        $client = static::createClient();
+        $client->request(
+            'POST',
+            'http://store.loc/api/statement/income',
+            [],
+            [],
+            $headers,
+            json_encode($postData)
+        );
+        $res = json_decode($client->getResponse()->getContent(), true);
+        $this->assertNotEquals(Response::HTTP_CREATED, $client->getResponse()->getStatusCode(), 'Выброшено исключение при создании проводки');
+        $this->assertIsNotArray($res);
     }
 
 
